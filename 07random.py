7e1b31315877238a44a4000d20fc60a89f61d4dd
Mohd.Anas <anaswebdev.pro@gmail.com>
2026-03-10 16:41:27 +0530

    Add random number generation and selection examples

---
+++ 07random.py
@@ -0,0 +1,18 @@
+import random 
+
+x = random.random()
+print(x)
+
+
+range = random.randrange(1, 10)
+print(range)
+
+
+list = ["apple", "banana", "cherry"]
+choice = random.choice(list)
+print(choice)
+shuffle = random.shuffle(list)
+print(list)
+
+
+
