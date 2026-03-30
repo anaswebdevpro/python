6f56a061286ef2d540bf68a8611497a6b4c8596a
Mohd.Anas <anaswebdev.pro@gmail.com>
2026-03-30 13:32:00 +0530

    Add a debug decorator to log function calls and their arguments

--- 06_Decorators/q2.py
+++ 06_Decorators/q2.py
@@ -1,6 +1,14 @@
 
 
 
+def debug(func):
+    def wrapper(*args, **kwargs):
+        print(f"Function '{func.__name__}' called with arguments: {args} and keyword arguments: {kwargs}")
+        return func(*args, **kwargs)
+    
+    return wrapper
+
+
 
 
 
@@ -20,5 +28,6 @@
 
 def greet(name,greeting="Hello"):
     return f"{greeting}, {name}!"
+
 
 greet("anas",greeting="Hi your father is here")
